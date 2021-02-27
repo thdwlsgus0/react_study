7f3b5a09510227f42767d9f0cdd59314d8195042
thdwlsgus0 <thdwlsgus0@naver.com>
2021-02-27 20:21:53 +0900

    webpack 설정

---
+++ lecture/끝말잇기/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  name:'wordrelay-setting',
+  mode:'development', // 실서비스 : production
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  
+  entry: {
+     app:['./client', './WordRelay'],
+  }, // 입력
+  module: {
+    rules:[{
+      test:/\.jsx?/,
+      loader: 'babel-loader',
+      options:{
+        presets:[
+           '@babel/preset-env',
+           '@babel/preset-react'
+        ],
+        plugins: ['@babel/plugin-proposal-class-properties']
+      }
+    }],  
+  },
+
+  output:{
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js'
+  }, // 출력
+};
